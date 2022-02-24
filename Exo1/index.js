1462b8f9b661be6a49da3975d3635c03f3eb6914
Louis Cavecchi <cavecchilouis@gmail.com>
2022-02-24 11:29:04 +0100

    Last push

--- Exo1/index.js
+++ Exo1/index.js
@@ -2,12 +2,13 @@
 
 let cards = document.querySelectorAll('.card')
 let modal = document.querySelector('.modal')
-console.log(cards)
+const displayModal = () => {
+    modal.classList.toggle('active')
+}
+
+document.querySelector('.modal h2').addEventListener('click', displayModal)
 
 cards.forEach(card => {
     card.addEventListener('click', displayModal)
 })
 
-const displayModal = () => {
-
-}
